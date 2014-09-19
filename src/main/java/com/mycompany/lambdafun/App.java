115aa658821cfca3ed533c2d429139ba2cb15ed5
Michael <Michael@Michael_Komp>
2014-09-19 23:03:18 +0200

    added NumericFunc Interface - solved

--- src/main/java/com/mycompany/lambdafun/App.java
+++ src/main/java/com/mycompany/lambdafun/App.java
@@ -4,13 +4,25 @@ package com.mycompany.lambdafun;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        LambdaTest t;
-        t = (i) -> (i*9);
-        t = (i) -> (i*90);
-        System.out.println( t.methodInt(10) );
+public class App {
+
+    public static void main(String[] args) {
+        {
+            LambdaTest t;
+            t = (i) -> (i * 9);
+            t = (i) -> (i * 90);
+            System.out.println("Lambda multiply test: " + t.methodInt(10));
+        }
+
+        {
+            NumericFunc factorial = (n) -> {
+                int result = 1;
+                for (int i = 1; i <= n; i++) {
+                    result = i * result;
+                }
+                return result;
+            };
+            System.out.println("Lambda factorial test: " + factorial.func(5));
+        }
     }
 }
